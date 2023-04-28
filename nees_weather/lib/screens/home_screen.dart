2b96c8e180810c2766bd77ad5dffda50d336a9f0
Guilherme Fonseca <guilherme.ramos@blubybs2.com>
2023-04-28 16:00:18 -0300

    finished home screen layout

---
+++ nees_weather/lib/screens/home_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:nees_weather/commons/app_colors.dart';
+import 'package:nees_weather/data/city_weather_model.dart';
+import 'package:nees_weather/data/weather_api_repository.dart';
+import 'package:nees_weather/widgets/app_bar.dart';
+import 'package:nees_weather/widgets/next_forecast_widget.dart';
+import 'package:nees_weather/widgets/small_info_widget.dart';
+import 'package:nees_weather/widgets/today_info_widget.dart';
+import 'package:nees_weather/widgets/weather_summary_widget.dart';
+
+class HomeScreen extends StatefulWidget with PreferredSizeWidget {
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreene();
+}
+
+class _HomeScreene extends State<HomeScreen> {
+  late Future<CityWeatherModel> city;
+
+  Future<CityWeatherModel> getCitytWeather() {
+    city = getWeather();
+    return city;
+  }
+
+  @override
+  Widget build(BuildContext context) => SafeArea(
+        child: FutureBuilder<CityWeatherModel>(
+          future: getCitytWeather(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return _body(snapshot);
+            } else if (snapshot.hasError) {
+              return const Center(
+                child: Text('Temos erro'),
+              );
+            } else {
+              return const CircularProgressIndicator();
+            }
+          },
+        ),
+      );
+
+  Widget _body(AsyncSnapshot<CityWeatherModel> snapshot) {
+    Results value = snapshot.data!.results;
+    return Scaffold(
+      backgroundColor: AppColors.backgroundBlue,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(kToolbarHeight),
+        child: AppBarWidget(cityName: value.cityName),
+      ),
+      body: ListView(
+        children: [
+          WeatherSummaryWidget(
+              forecast: snapshot.data!.results.forecast, value: value),
+          SmallInfoWidget(
+            value: value,
+          ),
+          TodayInfoWidget(value: value),
+          NextForecastWidget(forecast: snapshot.data!.results.forecast),
+        ],
+      ),
+    );
+  }
+}
