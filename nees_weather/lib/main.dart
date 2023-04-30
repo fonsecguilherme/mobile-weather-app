9f3f71e150c13e19fc92011c730895e0ff7df7c6
Guilherme Fonseca <guilherme.ramos@blubybs2.com>
2023-04-30 00:28:12 -0300

    added error screen route

--- nees_weather/lib/main.dart
+++ nees_weather/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nees_weather/screens/error_screen.dart';
 import 'package:nees_weather/screens/home_screen.dart';
 
 void main() {
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => const HomeScreen(),
+        '/errorScreen': (context) => const ErrorScreen(),
       },
     );
   }
