aa7c4ec7192b8515d9314b5ef1c59f5c3eb59d23
Guilherme Fonseca <guilherme.ramos@blubybs2.com>
2023-04-29 22:44:55 -0300

    added compass direction strings

--- nees_weather/lib/commons/app_strings.dart
+++ nees_weather/lib/commons/app_strings.dart
@@ -5,4 +5,13 @@ class AppStrings {
   static const String sunset = 'Pôr do sol';
   static const String time = 'Hora da atualização ';
   static const String windDirection = 'Direção do vento';
+  static const String north = 'Norte';
+  static const String south = 'Sul';
+  static const String east = 'Leste';
+  static const String west = 'Oeste';
+  static const String northeast = 'Nordeste';
+  static const String northwest = 'Noroeste';
+  static const String southeast = 'Sudeste';
+  static const String southwest = 'Sudoeste';
+  
 }
