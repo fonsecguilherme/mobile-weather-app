704fc86880008df1a826f0d4ded2906581eda38e
Guilherme Fonseca <guilherme.ramos@blubybs2.com>
2023-04-30 13:04:17 -0300

    defined new default app font

--- nees_weather/lib/main.dart
+++ nees_weather/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'NEES Weather',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Roboto'),
       initialRoute: '/',
       routes: {
         '/': (context) => const HomeScreen(),
